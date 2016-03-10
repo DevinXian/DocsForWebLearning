a15a10aa7c25dc06358b65b2317836ffa8cfcb48
DevinXian <devinxian@gmail.com>
2016-03-10 10:42:07 +0800

    js prototype

---
+++ javascript/__proto__/index.js
@@ -0,0 +1,23 @@
+/**
+ * 构造函数的Prototype通过new操作符会传递给每一个对象的__proto__
+ */
+Function.prototype.method = function (name, fn) {
+  this.prototype[name] = fn;
+  return this;
+};
+
+function Anim(a) {
+  this.value = 1;
+}
+
+Anim.method('hello', function () {
+  console.log(this.value);
+}).method('hi', function () {
+  console.log('hi');
+});
+
+
+console.log((new Anim()).__proto__);
+console.log((new Anim()).__proto__);//undefined
+(new Anim).hello();
+(new Anim).hi();
